897a15c39f2022a2de0cdc5f34bc20e8913a25a2
Kartheek <99461078+kartheek1104@users.noreply.github.com>
2025-07-29 23:44:44 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -2,9 +2,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/EmployeeDB";
-    private static final String USER = "root";
-    private static final String PASSWORD = "kartheek1"; // change this
+    private static final String URL = "your-jdbc-localhost-link"; // change this
+    private static final String USER = "your-username"; // change this
+    private static final String PASSWORD = "your-password"; // change this
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
